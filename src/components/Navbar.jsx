118354bc3e517fa935477d8545a49061c0eaec15
marcos <marcospiccotto12@gmail.com>
2022-08-01 08:09:26 -0300

    Base of the proyect with Vite

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+	return (
+		<div className="navbar">
+			<div className="navbar-title">
+				<img className="navbar-logo" src="src\assets\Trollface.png" alt="Troll face" />
+				<span>Meme Generator</span>
+			</div>
+            <span>React Course - Project 3</span>
+		</div>
+	);
+}
